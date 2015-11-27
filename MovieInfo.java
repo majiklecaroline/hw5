1c7b2a405ef0309821420d430e2f8952e58a3036
majiklecaroline <zhang.caroline@outlook.com>
2015-11-27 13:25:04 -0500

    BST hw 5
    death program

---
+++ MovieInfo.java
@@ -0,0 +1,14 @@
+
+public class MovieInfo {
+    
+	String shortName;
+	String fullName;
+	int ID;
+	
+	public MovieInfo(int id, String s, String f) {
+		ID = id;
+		shortName = s;
+		fullName = f;
+	}
+    
+}
